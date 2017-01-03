5c9727115df43c727679cfe99f33c65f192ebe99
54706424372 <54706424372@serpro-1531095>
2017-01-03 14:00:01 -0300

    v16

--- Alarm/src/main/java/org/marcosoft/alarm/Beeper.java
+++ Alarm/src/main/java/org/marcosoft/alarm/Beeper.java
@@ -1,17 +1,26 @@
 package org.marcosoft.alarm;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.swing.JOptionPane;
+
+import org.marcosoft.lib.AudioUtils;
+import org.marcosoft.lib.IOUtils;
 
 /**
  * Soar beep.
  */
 public class Beeper {
 
-	private BeepConfig beepConfig;
+	private final BeepConfig beepConfig;
 
 	private boolean toStop;
+
+    private boolean beepSoundCardError;
+
+    private ByteArrayInputStream alertBytes;
 
 	public Beeper(BeepConfig beepConfig) {
 		this.beepConfig = beepConfig;
@@ -22,40 +31,59 @@ public class Beeper {
 	}
 
 	public void beep() {
+	    beepSoundCardError = false;
 		toStop = false;
 		for (int countBeep = 0; countBeep < beepConfig.getTimes(); countBeep++) {
 			if (!beepConfig.isMute()) {
 				try {
+				    beepSoundCard();
 					execute(beepConfig.getCommand());
-				} catch (Exception e) {
+				} catch (final Exception e) {
 					JOptionPane.showMessageDialog(null, "I can't beep! " + e.getMessage());
 					break;
 				}
 			}
 			sleep(beepConfig.getPause());
-			if (toStop)
+			if (toStop) {
 				break;
+			}
 		}
 	}
+
+    private void beepSoundCard() {
+        if (beepSoundCardError) {
+            return;
+        }
+        try {
+            if (alertBytes == null) {
+                final InputStream stream = Beeper.class.getResourceAsStream("/alert.wav");
+                alertBytes = new ByteArrayInputStream(IOUtils.readContent(stream));
+            }
+            AudioUtils.playClip(alertBytes);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            beepSoundCardError = true;
+        }
+    }
 
 	private void sleep(int milis) {
 		try {
 			Thread.sleep(milis);
-		} catch (InterruptedException e) {
+		} catch (final InterruptedException e) {
 
 		}
 	}
 
 	/**
 	 * Executa o comando e espera pelo termino do processo.
-	 * 
+	 *
 	 * @param command
 	 *            comando
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	private void execute(String command) throws IOException, InterruptedException {
-		Process process = Runtime.getRuntime().exec(command);
+		final Process process = Runtime.getRuntime().exec(command);
 		process.waitFor();
 	}
 }
